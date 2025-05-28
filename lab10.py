8e4ae200d974c0ef40ea0ed69bc06b5fd3d15f5b
markiyaan <154061832+markiyaan@users.noreply.github.com>
2025-05-29 02:24:24 +0300

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 print("Це лабораторна робота №10")
 print("Jenkins успішно виконав скрипт!")
 print("Дата: 29.05.2025")
